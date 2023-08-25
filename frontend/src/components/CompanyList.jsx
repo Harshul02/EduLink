9fef462e7f5f475f8642cf65523fa0cdb2625211
Aayush <aayushbisht501@gmail.com>
2023-08-25 13:18:24 +0530

    browse companies page completed

--- frontend/src/components/CompanyList.jsx
+++ frontend/src/components/CompanyList.jsx
@@ -1,23 +1,28 @@
 // CompanyList.js
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import CompanyCard from './CompanyCard';
-import { io } from 'socket.io-client';
-import Chat from './Chat';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CompanyCard from "./CompanyCard";
+import { io } from "socket.io-client";
+import Chat from "./Chat";
 
 const CompanyList = () => {
   const [companyDataList, setCompanyDataList] = useState([]);
   const [socket, setSocket] = useState(null);
   const [selectedCompany, setSelectedCompany] = useState(null);
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleCollapse = () => {
+    setExpanded(!expanded);
+  };
 
   const getCompanyData = async () => {
     try {
-      const response = await axios.post('/api/college/getcompanydata');
+      const response = await axios.post("/api/college/getcompanydata");
       setCompanyDataList(response.data.data);
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   useEffect(() => {
     getCompanyData();
@@ -30,50 +35,172 @@ const CompanyList = () => {
   }, []);
 
   const handleCardClick = (companyData) => {
-    setSelectedCompany((prevSelected) => (prevSelected === companyData ? null : companyData));
+    setSelectedCompany((prevSelected) =>
+      prevSelected === companyData ? null : companyData
+    );
   };
 
   return (
-    <div className="container" style={{ width: '80%' }}>
+    <div className="container" style={{ width: "80%" }}>
       <div className="row">
-        <div className="col-md-6" style={{ overflowY: 'auto', maxHeight: '70vh' }}>
+        <div
+          className="col-md-6"
+          style={{ overflowY: "auto", maxHeight: "70vh" }}
+        >
           {companyDataList.map((companyData) => (
             <CompanyCard
               key={companyData._id}
               companyData={companyData}
-              loggedInUserId={localStorage.getItem('collegetoken')}
+              loggedInUserId={localStorage.getItem("collegetoken")}
               handleClick={() => handleCardClick(companyData)}
             />
           ))}
         </div>
         <div className="col-md-6">
-        {selectedCompany ? (
-  <div className="college-details card mb-3 border-0 shadow rounded" style={{ background: "linear-gradient(to right, #b7d8e8, #c7e9f4)" }}>
-    <div className="card-body">
+          {selectedCompany ? (
+            <div
+              className="college-details card mb-3 border-0 shadow rounded"
+              style={{
+                background: "linear-gradient(to right, #b7d8e8, #c7e9f4)",
+              }}
+            >
+              {/* <div className="card-body">
       <h3 className="card-title fs-4 mb-3 text-primary">{selectedCompany.companyName}</h3>
       <p className="card-text fs-6 mb-0 text-muted">
         {selectedCompany.companyDetail?.about && selectedCompany.companyDetail.about.length > 100
           ? selectedCompany.companyDetail.about.slice(0, 100) + "..."
           : selectedCompany.companyDetail?.about || "About"}
       </p>
-    </div>
-  </div>
-) : (
-  <div className="default-details card mb-3 border-0 shadow rounded" style={{ background: "#f0f0f0" }}>
-    <div className="card-body text-center py-5">
-      <i className="fas fa-building fs-3 mb-3 text-muted"></i>
-      <p className="card-text fs-5 fw-bold mb-0">Click on a Company to view details.</p>
-    </div>
-  </div>
-)}
+    </div> */}
+              <div class="row">
+                <div class="col-3 ">
+                  <div class="row justify-content-end">
+                    <div class="col-xl-11 col-sm-12 col-12">
+                      <div class="card rounded-4 mt-4">
+                        <div class="card-content">
+                          <div class="card-body">
+                            <div class="media d-flex">
+                              <div class="align-self-center">
+                                <i class="icon-user warning font-medium-3 float-left"></i>
+                              </div>
+                              <div class="media-body text-right float-end">
+                                <h5>
+                                  {selectedCompany.companyDetail.employees}
+                                </h5>
+                                <span>Total Students</span>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
 
-          {selectedCompany && (
+                  <div className="row justify-content-end ">
+                    <div class="col-xl-11 col-sm-12 col-12">
+                      <div class="card rounded-4">
+                        <div class="card-content">
+                          <div class="card-body">
+                            <div class="media d-flex">
+                              <div class="align-self-center">
+                                <i class="icon-pointer danger font-medium-3 float-left"></i>
+                              </div>
+                              <div class="media-body text-right">
+                                <h6>
+                                  {selectedCompany.companyDetail.location}
+                                </h6>
+                                <span>Location</span>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="row justify-content-end">
+                    <div className="col-xl-11 col-sm-12 col-12">
+                      <button
+                        class="btn btn-success"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapsedomain"
+                        aria-expanded="false"
+                        aria-controls="collapsedomain"
+                      >
+                        Courses Offered
+                      </button>
+                      <div class="collapse" id="collapsedomain">
+                        <div class="card card-body">
+                          {selectedCompany.companyDetail.domains}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class=" col-9 mt-4">
+                  <div
+                    className=" rounded-5 border-start bg-primary bg-opacity-10 bg-gradient fs-7 py-4 px-3 text-primary-emphasis "
+                    style={{ fontFamily: "'Roboto', sans-serif" }}
+                  >
+                    {/* <div class="card card-body"  style={{ background: "#198754" }}>{selectedCompany.companyName}</div> */}
+
+                    <div
+                      className="card mb-4  rounded-5"
+                      style={{ width: "50%", margin: "0 auto" }}
+                    >
+                      <div className="card-content rounded-5">
+                        <div className="card-body text-black bg-primary bg-opacity-10 bg-gradient rounded-5">
+                          <h5 className="card-title text-uppercase fw-bolder">
+                            {selectedCompany.companyName}
+                          </h5>
+                          <h6 className="card-subtitle text-uppercase fw-semibold">
+                            {selectedCompany.companyType}
+                          </h6>
+                        </div>
+                      </div>
+                    </div>
+
+                    <p>{expanded ? selectedCompany.companyDetail.about : selectedCompany.companyDetail.about.slice(0, 250)+"..."} {selectedCompany.companyDetail.about.length > 200 && (
+            <a
+              type="button"
+              onClick={toggleCollapse}
+            >
+              {expanded ? <i class="bi bi-caret-up-fill"></i> : <i class="bi bi-caret-down-fill"></i>}
+            </a>
+          )}</p>
+          
+          {expanded && (
+            <div className="collapse" id="collapseabout">
+              {selectedCompany.companyDetail.about}
+            </div>
+          )}
+                    {selectedCompany && (
             <Chat userType="college" loggedInUserId={localStorage.getItem('collegetoken')}  userId={selectedCompany._id} socket={socket} />
           )}
-        </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div
+              className="default-details card mb-3 border-0 shadow rounded"
+              style={{ background: "#f0f0f0" }}
+            >
+              <div className="card-body text-center py-5">
+                <i className="fas fa-building fs-3 mb-3 text-muted"></i>
+                <p className="card-text fs-5 fw-bold mb-0">
+                  Click on a Company to view details.
+                </p>
+              </div>
+            </div>
+          )}
 
+          {/* {selectedCompany && (
+            <Chat userType="college" loggedInUserId={localStorage.getItem('collegetoken')}  userId={selectedCompany._id} socket={socket} />
+          )} */}
+        </div>
       </div>
-      
     </div>
   );
 };
